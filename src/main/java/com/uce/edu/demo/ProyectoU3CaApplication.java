a829b6b31d26ce5427cd45e058dde7f75e883c71
Cristian Arboleda <cdarboleda@uce.edu.ec>
2022-08-05 20:21:30 -0500

    Tarea 26 Mapeo Inner Join, Left Join y Right Join

--- src/main/java/com/uce/edu/demo/ProyectoU3CaApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU3CaApplication.java
@@ -8,18 +8,17 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.uce.edu.demo.repository.modelo.Hotel;
-import com.uce.edu.demo.service.IHotelService;
+import com.uce.edu.demo.repository.modelo.Factura;
+import com.uce.edu.demo.service.IFacturaService;
 
 @SpringBootApplication
 public class ProyectoU3CaApplication implements CommandLineRunner{
 
 	private static final Logger logger = Logger.getLogger(ProyectoU3CaApplication.class);
 	
-//	@Autowired
-//	private IFacturaService facturaService;
 	@Autowired
-	private IHotelService hotelService;
+	private IFacturaService facturaService;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU3CaApplication.class, args);
@@ -29,41 +28,24 @@ public class ProyectoU3CaApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		//INNER
-//		logger.info("INNER JOIN");
-//		List<Factura> listaFactura = this.facturaService.buscarFacturaInnerJoin();
-//		listaFactura.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
-//		
-//		List<Factura> listaFactura2 = this.facturaService.buscarFacturaInnerJoin(15);
-//		listaFactura2.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
-//		
-//		//LEFT
-//		logger.info("LEFT JOIN");
-//		List<Factura> listaFacturaLeft = this.facturaService.buscarFacturaOuterLeftJoin();
-//		listaFacturaLeft.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
-//		List<Factura> listaFacturaLeft2 = this.facturaService.buscarFacturaOuterLeftJoin(15);
-//		listaFacturaLeft2.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));;
-//		
-//		//RIGHT
-//		logger.info("RIGHT JOIN");
-//		List<Factura> listaFacturaRight = this.facturaService.buscarFacturaOuterRightJoin(15);
-//		listaFacturaRight.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
-
-		//WHERE
-		logger.info("RELACIONAMIENTO WHERE");
-		List<Hotel> listaHotelWhere = this.hotelService.buscarHotelJoinWhere("Familiar");
-		listaHotelWhere.stream().forEach(x -> logger.info("Hotel: Nombre: "+ x.getNombre()+" Direccion: "+x.getDireccion()));
-		logger.info("\n");
+		logger.info("INNER JOIN");
+		List<Factura> listaFactura = this.facturaService.buscarFacturaInnerJoin();
+		listaFactura.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
 		
-		//INNER
-		logger.info("INNER JOIN EAGER/LAZY");
-		List<Hotel> listaHotel3 = this.hotelService.buscarHotelInnerJoin("Individual");
-		listaHotel3.stream().forEach(x -> logger.info("Hotel: "+ x.getHabitaciones()));
-		logger.info("\n");
+		List<Factura> listaFactura2 = this.facturaService.buscarFacturaInnerJoin(15);
+		listaFactura2.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
 		
-		//JOIN FETCH
-		logger.info("JOIN FETCH");
-		List<Hotel> listaHotelFetch = this.hotelService.buscarHotelJoinFetch("Individual");
-		listaHotelFetch.stream().forEach(x -> logger.info("Hotel: "+ x.getHabitaciones()));
+		//LEFT
+		logger.info("LEFT JOIN");
+		List<Factura> listaFacturaLeft = this.facturaService.buscarFacturaOuterLeftJoin();
+		listaFacturaLeft.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
+		List<Factura> listaFacturaLeft2 = this.facturaService.buscarFacturaOuterLeftJoin(15);
+		listaFacturaLeft2.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));;
+		
+		//RIGHT
+		logger.info("RIGHT JOIN");
+		List<Factura> listaFacturaRight = this.facturaService.buscarFacturaOuterRightJoin(15);
+		listaFacturaRight.stream().forEach(x -> logger.info("Factura: Fecha: "+ x.getFecha()+" Numero: "+x.getNumero()));
 	
 	}
 
